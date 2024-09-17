718192e1541f1b49dc819f872250873949976046
James Ybanez <ybanezjames17@gmail.com>
2024-09-17 18:17:08 +1200

    Complete tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  const celcius = (temp - 32) * (5/9);
+  const rounded = Math.round(celcius * 10) / 10;
+  return rounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  const fahrenheit = temp * (9/5) + 32;
+  const rounded = Math.round(fahrenheit * 10) / 10;
+  return rounded;
 };
 
 // Do not edit below this line
