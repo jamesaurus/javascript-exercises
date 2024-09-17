1585ce3d2cdb208bc0b06b4f0fc67babb14d6f04
James Ybanez <ybanezjames17@gmail.com>
2024-09-17 17:18:55 +1200

    Complete 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
 
+    if (!Number.isInteger(num1) || !Number.isInteger(num2)) { return "ERROR"; }
+    if (num1 < 0 || num2 < 0) { return "ERROR"; }
+
+    let start = num1;
+    let end = num2;
+    if (num1 > num2) {
+        start = num2;
+        end = num1;
+    }
+
+    let total = 0;
+    for (let i = start; i <= end; i++) {
+        total += i;
+    }
+    return total;
 };
 
 // Do not edit below this line
