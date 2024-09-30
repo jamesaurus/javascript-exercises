48e8ce2fc7f08c91ceafaf45f5884d73ea54e898
James Ybanez <ybanezjames17@gmail.com>
2024-09-30 17:37:32 +1300

    Complete 12_findtheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,29 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const date = new Date();
+    const currentYear = date.getFullYear();
 
+    people.sort((person1, person2) => {
+
+        // Check if person1 has died
+        if (person1.yearOfDeath === undefined) {
+            person1Age = currentYear - person1.yearOfBirth;
+        }
+        else {
+            person1Age = person1.yearOfDeath - person1.yearOfBirth;
+        }
+
+        // Check if person2 has died
+        if (person2.yearOfDeath === undefined) {
+            person2Age = currentYear - person2.yearOfBirth;
+        }
+        else {
+            person2Age = person2.yearOfDeath - person2.yearOfBirth;
+        }
+
+        return person1Age >= person2Age ? -1 : 1;
+    });
+    
+    return people[0];
 };
 
 // Do not edit below this line
