6e3e964e2c4f56a73b7c030245319e3bb7ae69b9
James Ybanez <ybanezjames17@gmail.com>
2024-09-30 17:10:16 +1300

    Complete 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let precedingOne = 1;
+    let precedingTwo = 1; 
+    let member = 1;
 
+    if (num == 0) { return 0; } 
+    if (num < 0) { return "OOPS"; }
+
+    for (let i = 2; i < num; i++) {
+        member = precedingOne + precedingTwo;
+        precedingOne = precedingTwo;
+        precedingTwo = member;
+    }
+
+    return member; 
 };
 
 // Do not edit below this line
