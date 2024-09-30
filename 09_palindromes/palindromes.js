db4c06280df4ba74a77351233e30d9903fac4ba6
James Ybanez <ybanezjames17@gmail.com>
2024-09-30 16:41:13 +1300

    Complete 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    const alphabetNumeric = "abcdefghijklmnopqrstuvwxyz0123456789"
+    
+    // Clean the string
+    const cleanedString = string.toLowerCase()
+                                .split("")
+                                .filter((char) => alphabetNumeric.includes(char))
+                                .join("");
 
+    // Create reverse of the cleaned string
+    const reverseString = cleanedString.split("").reverse().join("");
+
+    // Check if its a palindrome
+    return cleanedString === reverseString;
 };
 
 // Do not edit below this line
